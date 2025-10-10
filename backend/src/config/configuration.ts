70b0c193ea36fc7c811c6399b631becb47e86139
yun <yun_132@qq.com>
2025-10-10 12:03:40 +0800

    feat(config): 优化webRoot配置路径处理和默认值设置

--- backend/src/config/configuration.ts
+++ backend/src/config/configuration.ts
@@ -1,3 +1,4 @@
+import { join } from 'path';
 export const configuration = () => ({
   port: parseInt(process.env.PORT || '3000'),
   nodeEnv: process.env.NODE_ENV || 'production',
@@ -6,5 +7,5 @@ export const configuration = () => ({
   logLevel: process.env.LOG_LEVEL,
   initTestData: process.env.INIT_TEST_DATA === 'true' || false,
   isPkg: (process as any).pkg !== undefined,
-  webRoot: process.env.WEB_ROOT || 'public',
+  webRoot: join(__dirname, process.env.WEB_ROOT!),
 });
