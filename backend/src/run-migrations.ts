6811e58bc86dd1e4879328729599f042963eab70
yun <yun_132@qq.com>
2025-10-14 09:20:00 +0800

    refactor(migration): 优化迁移文件处理和打包配置

--- backend/src/run-migrations.ts
+++ backend/src/run-migrations.ts
@@ -1,16 +1,17 @@
 import { DataSource } from 'typeorm';
-import * as path from 'path';
+import { resolve, join } from 'path';
 import * as fs from 'fs';
 import { Init } from './init/init.entity';
 import { User } from './modules/users/user.entity';
 import { Link } from './modules/links/link.entity';
 
+const migDir = resolve(__dirname, './migrations');
 
 const migrations = fs
-  .readdirSync(path.join(__dirname, './migrations'))
+  .readdirSync(migDir)
   .filter(f => f.endsWith('.js'))
   .map(f => {
-    const mod = require(path.join(__dirname, './migrations', f));
+    const mod = require(join(__dirname, './migrations', f));
     return Object.values(mod)[0];   // 第一个导出就是类
   })
   .filter(Boolean) as (string | Function)[];
